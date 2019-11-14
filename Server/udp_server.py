135d34cf33f647215882f0f8d02dc27cc0e6e821
James Baak <jamesbaak@protonmail.com>
2019-11-14 01:56:03 -0500

    James [feat] - Updated comments for server and added load function

--- Server/udp_server.py
+++ Server/udp_server.py
@@ -11,9 +11,10 @@ ACK   = { "type": "ack", "payload": "" }
 
 class MyUDPServer(socketserver.UDPServer):
     
-    def __init__(self, server_address, RequestHandlerClass, bind_and_activate, db_file):
+    def __init__(self, server_address, RequestHandlerClass, bind_and_activate, db_file, ml_addr):
         super().__init__(server_address, RequestHandlerClass, bind_and_activate)
         self.database = Database(db_file)
+        self.ml_addr = ml_addr
 
 class MyUDPHandler(socketserver.BaseRequestHandler):
 
@@ -22,7 +23,9 @@ class MyUDPHandler(socketserver.BaseRequestHandler):
         socket = self.request[1]
         print("Request from {}:".format(self.client_address[0]))
         print(request)
+        print(self.client_address)
 
+        # Check to see if type and payload are in the json message request to the server
         if not "type" in request:
             socket.sendto(
                 bytes(json.dumps(self.__constructError__("Missing type field...")), "utf-8"),
@@ -33,12 +36,15 @@ class MyUDPHandler(socketserver.BaseRequestHandler):
                 bytes(json.dumps(self.__constructError__("Missing payload field...")), "utf-8"),
                 self.client_address
             )
+        # Valid request structure, so handle it if valid type, else reply with error message
         else:
             # Process request
             if request["type"] == "sample": # Send request to ML PI to sample and return response
                 self.__handleSample__(socket, request["payload"])
             elif request["type"] == "save": # From the ML PI with the ML result record
                 self.__handleSave__(socket, request["payload"])
+            elif request["type"] == "load": # Load the users data into the Machine learning PI
+                self.__handLoad__(socket, request["payload"])
             else: # Request type not specified
                 socket.sendto(
                     bytes(json.dumps(self.__constructError__("Unknown type field...")), "utf-8"),
@@ -46,19 +52,47 @@ class MyUDPHandler(socketserver.BaseRequestHandler):
                 )
 
     def __constructError__(self, msg):
+        """
+        Construct an error json for UDP response
+        msg [string] - The payload of the error message
+        """
         error0 = ERROR.copy()
         error0["payload"] = msg
         return error0
 
     def __handleSample__(self, socket, payload):
+        """
+        Sample the bracelet and get ML prediction
+        payload [string] - Empty string to get prediction, and int representing gesture
+        """
+        # Can access the ML PI address through self.server.ml_addr
+        self.__sendAck__(socket)
         print(payload)
 
     def __handleSave__(self, socket, payload):
+        """
+        Should write the ML event result to the MLResults database 
+        payload [json: { "prediction": string, "class": string, "input": int[196] }]
+        """
         # Can access the database object through self.server.database
         print(payload)
+               
+    def __handleLoad__(self, socket, payload):
+        """
+        Send the user's data records to the ML PI for training
+        payload [string] - The username of the user to load the ML PI with. If blank string (""),
+                        then load all data records.
+        """
+        self.__sendAck__(socket)
+        print(payload)
+
+    def __sendAck__(self, socket):
+        socket.sendto(bytes(json.dumps(ACK), "utf-8"), self.client_address)
 
 if __name__ == "__main__":
     HOST, PORT = "", 9999
+    ML_HOST, ML_PORT = "localhost", 1024
     DB_LOC = r".\Database\aslr_database.db" # For windows. The slashes may have to be changed for linux
-    with MyUDPServer((HOST, PORT), MyUDPHandler, True, DB_LOC) as server:
+                                            # We can also check OS and change DB_LOC var respectivly
+    with MyUDPServer((HOST, PORT), MyUDPHandler, True, DB_LOC, (ML_HOST, ML_PORT)) as server:
         server.serve_forever()
